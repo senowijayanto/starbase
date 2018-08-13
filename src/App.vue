c0660ea7795cd01d56bfef3ac71bc42a0d355173
Seno Wijayanto <senowijayanto@gmail.com>
2018-08-13 23:20:18 +0700

    Fetch character and switch character

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <div id="app">
     <h3>{{title}}</h3>
-    <Character />
+    <Character
+      v-for="(id, index) in initial_ids"
+      :id="id"
+      :key="index + '-char'"
+    />
   </div>
 </template>
 
 <script>
   import Character from './components/Character.vue'
-  
+
   export default {
     name: 'app',
     data() {
       return {
-        title: 'Generate Your Team'
+        title: 'Generate Your Team',
+        initial_ids: [1, 13, 14]
       }
     },
     components: {
